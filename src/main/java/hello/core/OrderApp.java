c4a407fccf72c9a34462e3ed148e22d8982d0a13
gyuseon25 <mn5592@naver.com>
2024-03-05 17:23:27 +0900

    새로운 구조와 할인 정책 적용 ~ 스프링으로 전환하기

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -7,21 +7,28 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
         AppConfig appConfig = new AppConfig();
 //        MemberService memberService = new MemberServiceImpl();
-        MemberService memberService = appConfig.memberService();
+//        MemberService memberService = appConfig.memberService();
 //        OrderService orderService = new OrderServiceImpl();
-        OrderService orderService = appConfig.orderService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         long memberId = 1L;
         Member member = new Member(memberId, "itemA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Order order = orderService.createOrder(memberId, "itemA", 20000);
 
         System.out.println("order = " + order);
     }
